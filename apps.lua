9dbd827eea67478131d7fc47abdc5cbc3cbd3197
Ale2610 <aleproto00@gmail.com>
2014-10-04 12:21:17 +0200

    added games

--- apps.lua
+++ apps.lua
@@ -58,41 +58,35 @@ print("ID: " .. os.getComputerID())
 if os.getComputerLabel() then
 	print(" Name: " .. os.getComputerLabel())
 end
-term.setTextColor(colors.yellow)
 
 term.setCursorPos(2,7)
-print("[1] File Browser\n [2] LuaIDE\n [3] SertexGPS\n [4] Back")
+term.setTextColor(colors.lime)
+print("[1] Games")
+term.setTextColor(colors.yellow)
+print(" [2] File Browser\n [3] LuaIDE\n [4] SertexGPS\n [5] Back")
 
 
 
 while true do
   local id,key = os.pullEvent("key")
 	
-	if key == 29 then
-		print("CTRL Is Disabled!")
-		sleep(1)
-		shell.run(".sertexos/apps")
+	if key )) 2 then --1
+	  shell.run(".sertexos/games")
 	end
 	
-	if key == 157 then
-		print("CTRL Is Disabled!")
-		sleep(1)
-		shell.run(".sertexos/apps")
-	end
-	
-	if key == 2 then  --1
+	if key == 3 then  --2
 	  shell.run("fg", ".sertexos/fileBrowser")
     end
 	
-	if key == 3 then --2
+	if key == 4 then --3
 		shell.run("fg", ".sertexos/luaide")
 	end
 	
-	if key == 4 then --3
+	if key == 5 then --4
 		shell.run("fg", ".sertexos/sertexgps")
 	end
    
-    if key == 5 then  --4
+    if key == 6 then  --5
       shell.run(".sertexos/home")
     end
 	

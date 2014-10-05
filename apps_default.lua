72d87ddc45dae984152344f0be4c2da3964a02e6
Ale2610 <aleproto00@gmail.com>
2014-10-05 18:13:38 +0200

    temporary update

--- apps_default.lua
+++ apps_default.lua
@@ -15,22 +15,30 @@ sertexosapi.headerSub("Applications")
 term.setTextColor(colors.green)
 print("[1] Games")
 term.setTextColor(colors.blue)
-print(" [2] SertexGPS\n [3] Back")
+print(" [2] File Browser\n [3] LuaIDE\n [4] SertexGPS\n [5] Back")
 
 
 
 while true do
   local id,key = os.pullEvent("key")
 	
-	if key == 2 then
+	if key == 2 then --1
 	  shell.run(".sertexos/games")
 	end
 	
-	if key == 3 then
+	if key == 3 then  --2
+	  shell.run("fg", ".sertexos/fileBrowser")
+    end
+	
+	if key == 4 then --3
+		shell.run("fg", ".sertexos/luaide")
+	end
+	
+	if key == 5 then --4
 		shell.run("fg", ".sertexos/sertexgps")
 	end
    
-    if key == 4 then
+    if key == 6 then  --5
       shell.run(".sertexos/home")
     end
 	

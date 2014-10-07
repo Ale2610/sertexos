6cec9fff27b04d5c7dd26b5607acccac954c6c93
Ale2610 <aleproto00@gmail.com>
2014-10-07 18:29:36 +0200

    update

--- sertexapi.lua
+++ sertexapi.lua
@@ -7,6 +7,13 @@ function right(y, text )
         write(text)
 end
 
+function left(y, text)
+	h, w = term.getCursorPos()
+	term.setCursorPos(1, y)
+	write( text )
+	term.setCursorPos(h, w)
+end
+
 function center(y, text )
         local w, h = term.getSize()
         term.setCursorPos((w - #text) / 2, y)
@@ -20,5 +27,6 @@ function centerDisplay( text )
 end
 
 right(5, "example")
+left(3, "example")
 center(7, "example")
 centerDisplay("example")

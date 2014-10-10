7e75554ef4fdbf8d5b1ccabaac5490cb2cb45985
Ale2610 <aleproto00@gmail.com>
2014-10-10 22:08:02 +0200

    Update!

--- apps.lua
+++ apps.lua
@@ -8,14 +8,14 @@ sertexosapi.start()
 --Interface
 
 if not http then
-print("You need HTTP API enabled!")
-else
+error("You need HTTP API enabled!")
+end
  
 sertexosapi.headerSub("Applications")
 term.setTextColor(colors.green)
 print("[1] Games")
 term.setTextColor(colors.blue)
-print(" [2] Ice-Browser\n [3] SertexGPS\n [4] Back")
+print(" [2] Ice-Browser\n [3] SertexGPS\n [4] LuaIDE\n [5] Firewolf\n [6] Back")
 
 
 
@@ -33,10 +33,17 @@ while true do
 	if key == 4 then
 		shell.run("fg", ".sertexos/sertexgps")
 	end
+	
+	if key == 5 then
+		shell.run("fg", ".sertexos/luaide")
+	end
+	
+	if key == 6 then
+		shell.run("fg", ".sertexos/firewolf")
+	end
    
-    if key == 5 then
+    if key == 7 then
       shell.run(".sertexos/home")
     end
 	
-end
 end

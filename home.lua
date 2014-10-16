18ec846663f9dd33a6e3ac2fb7cfcd0b0f3c4d0a
Ale2610 <ale.proto00@gmail.com>
2014-10-16 12:02:49 +0200

    Update home.lua

--- home.lua
+++ home.lua
@@ -1,3 +1,27 @@
 os.pullEvent = os.pullEventRaw
+--Load
+os.loadAPI(".sertexos/apis/sertexosapi")
 
-print("Updating...")
+sertexosapi.load()
+sertexosapi.start()
+
+--Header and Menu
+
+sertexosapi.header()
+print(" [1] Applications")
+print(" [2] Options")
+print(" [3] Shell")
+print(" [4] Info/Help")
+print(" [5] Lock")
+print(" [6] Reboot")
+print(" [7] Shutdown")
+
+while true do
+
+  local id, key = os.pullEvent("key")
+
+  if key == 2 then
+    shell.run(".sertexos/apps")
+  end
+
+end

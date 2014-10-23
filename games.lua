5bd22b6ccb2857ff0b1e4c7c69e2c922ffd3562e
Ale2610 <ale.proto00@gmail.com>
2014-10-23 07:57:08 +0200

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,6 +1,7 @@
 os.pullEvent = os.pullEventRaw
 os.loadAPI(".sertexos/apis/sertexosapi")
-
+sertexosapi.load()
+sertexosapi.start()
 sertexosapi.headerSub("Games")
 
 print(" [1] Worm\n [2] Redirection\n [3] Adventure\n [4] Back")

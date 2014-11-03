b518a99c14f7bc69e8626c97171f615467b978dd
Ale2610 <Ale2610>
2014-11-03 18:30:02 +0100

    update!

--- boot.lua
+++ boot.lua
@@ -1,54 +1,37 @@
 os.pullEvent = os.pullEventRaw
 os.loadAPI(".sertexos/apis/sertexos")
-
 sertexos.loadWithSHA()
 sertexos.start()
 
-function pass()
-if fs.exists(".sertexos/.data/pass") then
-	term.clear()
-	term.setCursorPos(2,2)
-	term.setTextColor( title )
-	print(" SertexOS")
-	print""
-	while true do
-		write(" Insert Password: ")
-		local pass = read("*")
-		local f = fs.open(".sertexos/.data/pass", "r")
-		local crypt = sha256.sha256(pass)
-		if crypt == f.readLine() then
-			f.close()
-			shell.run(".sertexos/home")
-		else
-			print(" Wrong Password!")
-			sleep(2)
+function load()
+	if fs.exists(".sertexos/.data/pass") then
+		password = fs.open(".sertexos/.data/pass", "r")
+		
+		term.clear()
+		
+		while true do
+			term.setTextColor( red )
+			sertextext.slowCenterDisplay("Password: ")
+			input = read("*")
 			
+			if sha256.sha256( input ) == password.readLine() then
+				shell.run(".sertexos/home")
+			else
+				sertextext.slowCenter(4, "Wrong Password!")
+			end
 		end
+	else
+		shell.run(".sertexos/home")
 	end
-else
-	shell.run(".sertexos/home")
 end
-end
-
-if not http then
-  error("You need HTTP API enabled!")
-end
-
-term.clear()
-term.setCursorPos(2,2)
-term.setTextColor( logo )
-print("  # ")
-print(" #  ")
-print(" ###")
-term.setCursorPos(2,2)
+term.setTextColor( title )
 sertextext.center(2, "SertexOS")
 term.setTextColor( text )
-sertextext.centerDisplay("Loading...")
+sertextext.slowCenterDisplay("Loading...")
 sleep(1.8)
-
 
 if fs.exists(".sertexos/setup") then
 	shell.run(".sertexos/setup")
 end
 
-pass()
+load()
--- sertexos.lua
+++ sertexos.lua
@@ -1,10 +1,12 @@
 function load()
 	os.loadAPI("/.sertexos/apis/sertextext")
+	version = fs.open(".sertexos/ver", "r")
 end
 
 function loadWithSHA()
 	os.loadAPI("/.sertexos/apis/sertextext")
 	os.loadAPI("/.sertexos/apis/sha256")
+	version = fs.open(".sertexos/ver", "r")
 end
 
 function start()
@@ -14,21 +16,54 @@ function start()
 		dir = colors.green
 		app = colors.blue
 		bg = colors.white
-              logo = colors.red
+        logo = colors.red
+		
+		white = colors.white
+		orange = colors.orange
+		magenta = colors.magenta
+		lightBlue = colors.lightBlue
+		yellow = colors.yellow
+		lime = colors.lime
+		pink = colors.pink
+		gray = colors.gray
+		lightGray = colors.lightGray
+		cyan = colors.cyan
+		purple = colors.purple
+		blue = colors.blue
+		brown = colors.brown
+		green = colors.green
+		red = colors.red
+		black = colors.black
 	else
 		title = colors.white
 		text = colors.white
 		dir = colors.white
 		app = colors.white
 		bg = colors.black
-              logo = colors.white
+        logo = colors.white
+		
+		white = colors.white
+		orange = colors.white
+		magenta = colors.white
+		lightBlue = colors.white
+		yellow = colors.white
+		lime = colors.white
+		pink = colors.white
+		gray = colors.white
+		lightGray = colors.white
+		cyan = colors.white
+		purple = colors.white
+		blue = colors.white
+		brown = colors.white
+		green = colors.white
+		red = colors.white
+		black = colors.black
 	end
 	term.setBackgroundColor( bg )
 	term.clear()
 end
 
 function header()
-version = fs.open(".sertexos/ver", "r")
 term.clear()
 term.setTextColor( title )
 sertextext.center(2, "SertexOS")
@@ -43,7 +78,6 @@ term.setTextColor( app )
 end
 
 function headerSub( text )
-version = fs.open(".sertexos/ver", "r")
 term.clear()
 term.setTextColor(title)
 sertextext.center(2, "SertexOS")

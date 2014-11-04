8edafb367d36b00370b2b3c46b766cff194dff06
Ale2610 <ale.proto00@gmail.com>
2014-11-04 13:17:26 +0100

    Update sertexgps.lua

--- sertexgps.lua
+++ sertexgps.lua
@@ -1,32 +1,35 @@
 os.pullEvent = os.pullEventRaw
+os.loadAPI(".sertexos/apis/sertexos")
+sertexos.load()
+sertexos.start()
 
 term.clear()
 term.setCursorPos(1,1)
-term.setTextColor(colors.red)
+term.setTextColor( red )
 print""
 print(" SertexOS GPS")
 print""
 local x, y, z = gps.locate(5)
 if not x then
-	term.setTextColor(colors.red)
+	term.setTextColor( red )
 	textutils.slowPrint(" Failed to get your position!")
 	print(" Reason: No modem or no GPS Tower!")
 	print""
-	term.setTextColor(colors.yellow)
+	term.setTextColor( blue )
 	print(" Press Any Key!")
 	os.pullEvent("key")
 	term.clear()
 	shell.run("exit")
 else
-	term.setTextColor(colors.yellow)
+	term.setTextColor( blue )
 	textutils.slowPrint(" Position:")
 	print""
-	term.setTextColor(colors.lime)
+	term.setTextColor( green )
 	textutils.slowPrint(" x: " .. x)
 	textutils.slowPrint(" y: " .. y)
 	textutils.slowPrint(" z: " .. z)
 	print""
-	term.setTextColor(colors.yellow)
+	term.setTextColor( blue )
 	print(" Press Any Key!")
         os.pullEvent("key")
         term.clear()

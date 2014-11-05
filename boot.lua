66b675321e844c410225b6e1a8d2b81a3efc7c6c
Ale2610 <ale.proto00@gmail.com>
2014-11-05 23:51:07 +0100

    Update boot.lua

--- boot.lua
+++ boot.lua
@@ -29,14 +29,14 @@ term.setTextColor( title )
 sertextext.center(2, "SertexOS")
 term.setTextColor( text )
 sertextext.slowCenterDisplay("Loading...")
-sleep(1.8)
-
-recentVersion = fs.open(".sertexos/recentVersion", "w")
-recentVersion.write(http.get("").readLine())
-recentVersion.close()
 
 if fs.exists(".sertexos/setup") then
 	shell.run(".sertexos/setup")
 end
+
+recentVersion = fs.open(".sertexos/recentVersion", "w")
+recentVersion.write(http.get("https://raw.githubusercontent.com/Sertex-Team/sertexos/master/ver.txt").readLine())
+recentVersion.close()
+
 
 load()

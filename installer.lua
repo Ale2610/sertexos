d2c9f1d6dc7d686797f5e352c782b083514074e4
Ale2610 <Ale2610>
2014-12-16 16:34:57 +0100

    fix

--- installer.lua
+++ installer.lua
@@ -6,56 +6,19 @@ end
 	check = fs.open(".sertexos/check", "w")
 	check.write("false")
 	check.close
-
+	
+local bg, red, green, blue
 if term.isColor() then
-		title = colors.red
-		text = colors.green
-		dir = colors.green
-		app = colors.blue
-		bg = colors.white
-    logo = colors.red
-		
-		white = colors.white
-		orange = colors.orange
-		magenta = colors.magenta
-		lightBlue = colors.lightBlue
-		yellow = colors.yellow
-		lime = colors.lime
-		pink = colors.pink
-		gray = colors.gray
-		lightGray = colors.lightGray
-		cyan = colors.cyan
-		purple = colors.purple
-		blue = colors.blue
-		brown = colors.brown
-		green = colors.green
-		red = colors.red
-		black = colors.black
-	else
-		title = colors.white
-		text = colors.white
-		dir = colors.white
-		app = colors.white
-		bg = colors.black
-    logo = colors.white
-		
-		white = colors.white
-		orange = colors.white
-		magenta = colors.white
-		lightBlue = colors.white
-		yellow = colors.white
-		lime = colors.white
-		pink = colors.white
-		gray = colors.white
-		lightGray = colors.white
-		cyan = colors.white
-		purple = colors.white
-		blue = colors.white
-		brown = colors.white
-		green = colors.white
-		red = colors.white
-		black = colors.black
-	end
+	bg = colors.white
+	red = colors.red
+	green = colors.green
+	blue = colors.blue
+else
+	bg = colors.black
+	red = colors.white
+	green = colors.white
+	blue = colors.white
+end
 
 function clear()
 		term.setBackgroundColor(bg)
@@ -176,7 +139,7 @@ Reset()
 print("SertexOS Downloaded Successfully!")
 term.setTextColor(red)
 print("\nPress Any Key")
-term.setTextColor(white)
+term.setTextColor(colors.white)
 os.pullEvent("key")
 sleep(0,1)
 term.clear()
